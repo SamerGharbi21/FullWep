8edde297b506c2e62b9e5ef06efa837910b0c95c
SamerGharbi21 <Gharbisamer2@gmail.com>
2023-11-03 19:29:16 +0300

    init

--- index.php
+++ index.php
@@ -10,13 +10,22 @@ if(isset($_POST['submit'])){
 $sql = "INSERT INTO users(fname , lname , email)
         VALUE ('$fname' , '$lname' , '$email')"; 
 
-if(mysqli_query($mysqli , $sql)){
-    echo 'success';
-}else {
-    echo "Error creating new record: " . mysqli_error($mysqli);
+if(empty($fname)){
+    echo "First name empty" ;
+}elseif(empty($lname)) {
+    echo "Last name empty" ;
+}elseif(empty($email)){
+    echo "Email empty" ;
+}elseif(!filter_var($email , FILTER_VALIDATE_EMAIL)){
+    echo "Email is incorrect";
+}else{
+    if(mysqli_query($mysqli , $sql)){
+        header ('Location: index.php');
+    }else {
+        echo "Error creating new record: " . mysqli_error($mysqli);
+    }
 }
 }
-
 ?>
 
 <!DOCTYPE html>
