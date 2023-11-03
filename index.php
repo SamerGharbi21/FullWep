0d85045eee612971b963615efd3d101956b1e440
SamerGharbi21 <Gharbisamer2@gmail.com>
2023-11-03 19:35:34 +0300

    new

--- index.php
+++ index.php
@@ -1,31 +1,8 @@
 <?php 
 include './inc/conn.php';
+include './inc/form.php';
 
 
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-
-if(isset($_POST['submit'])){
-$sql = "INSERT INTO users(fname , lname , email)
-        VALUE ('$fname' , '$lname' , '$email')"; 
-
-if(empty($fname)){
-    echo "First name empty" ;
-}elseif(empty($lname)) {
-    echo "Last name empty" ;
-}elseif(empty($email)){
-    echo "Email empty" ;
-}elseif(!filter_var($email , FILTER_VALIDATE_EMAIL)){
-    echo "Email is incorrect";
-}else{
-    if(mysqli_query($mysqli , $sql)){
-        header ('Location: index.php');
-    }else {
-        echo "Error creating new record: " . mysqli_error($mysqli);
-    }
-}
-}
 ?>
 
 <!DOCTYPE html>
