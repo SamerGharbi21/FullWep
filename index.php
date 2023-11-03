aa6689e441baa851158ac87ec82a86cdf2bb7351
SamerGharbi21 <Gharbisamer2@gmail.com>
2023-11-03 17:35:10 +0300

    init

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
-<?php
-phpinfo();
+<?php 
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+
+if(isset($_POST['submit'])){
+echo $fname . " ss " . $lname . " ss " . $email ;
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css"> 
+    <title>Document</title>
+</head>
+<body>
+
+<form action="index.php" method="POST">
+    <input type="text" name="fname" id="fname" placeholder="First Name">
+    <input type="text" name="lname" id="lname" placeholder="Last Name">
+    <input type="text" name="email" id="email" placeholder="Email">
+    <input type="submit" name="submit" value="Send">
+</form>
+
+
+
+<script src="./js/script.js"></script>
+</body>
+</html>
