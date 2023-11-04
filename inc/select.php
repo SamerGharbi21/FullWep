ea0522587d4239712ffc6f488fce14fc4bcd8dd5
SamerGharbi21 <Gharbisamer2@gmail.com>
2023-11-05 00:27:37 +0300

    end

--- inc/select.php
+++ inc/select.php
@@ -1,5 +1,5 @@
 <?php 
-$sql = 'SELECT * FROM users' ;
+$sql = 'SELECT * FROM  users ORDER BY RAND() LIMIT 1 ' ;
 $result = mysqli_query($mysqli , $sql);
 $users = mysqli_fetch_all($result , MYSQLI_ASSOC);
 ?>
--- index.php
+++ index.php
@@ -9,17 +9,18 @@ include './inc/db_close.php';
 <?php include_once './parts/header.php'; ?> 
 
    
-    <div class="position-relative overflow-hidden p-3 p-md-5 m-mb-3 text-white rounded bg-dark">
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-mb-3 text-white text-center rounded bg-dark">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
       <h1 class="display-6 fw-normal">اربح مع سامر</h1>
       <p class="lead my-3">باقي على فتح التسجيل</p>
+      <p id="demo"></p>
       <p class="lead my-3">للسحب على ربح نسخة مجانية من برنامج</p>
       <p class="lead mb-0"><a href="#" class="text-white fw-bold">Coming Soon...</a></p>
     </div>
   </div>
 
 
-    <form class="mt-5" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+    <form class="mt-5 text-center" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
         <h3> الرجاء ادخال المعلومات </h3>
 
   <div class="mb-3">
@@ -34,7 +35,7 @@ include './inc/db_close.php';
     <div  class="form-text error"><?php echo $errors['LnameError'] ?></div>
   </div>
 
-  <div class="mb-3">
+  <div class="mb-3 my-3">
     <label for="Email" class="form-label">البريد الالكتروني</label>
     <input type="text" class="form-control" name="email" id="Email" value="<?php echo $email?>">
     <div class="form-text error"><?php echo $errors['EmailError'] ?></div>
@@ -42,16 +43,19 @@ include './inc/db_close.php';
   <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 </form>
 
-   <div class="row mb-5 pb-5">
+
+<button id="winner" type="button">اختيار الرابح</button>
+
+   <div id="cards" class="row mb-5 pb-5 text-center">
 <?php foreach($users as $user): ?>
     <div class="col-sm-6 my-2">
         <div class="card">
             <div class="card-body">
-    <h6 class="card-tittle"> <?php echo htmlspecialchars ($user['fname']) . ' ' .  htmlspecialchars ($user['lname']) ?> </h6>
-        <p class="card-text"> <?php echo  htmlspecialchars ($user['email']) ?></p>
+               <h6 class="card-tittle"> <?php echo htmlspecialchars ($user['fname']) . ' ' .  htmlspecialchars ($user['lname']) ?> </h6>
+            <p class="card-text"> <?php echo  htmlspecialchars ($user['email']) ?></p>
+         </div>
     </div>
-    </div>
-    </div>
+</div>
 
         <?php endforeach; ?>
 
